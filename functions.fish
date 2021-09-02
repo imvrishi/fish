455e0c5abadbea1c8c540f1e2e74b0f8442b7cbc
Rishi <imvrishi@gmail.com>
2021-09-02 17:31:58 +0530

    Update functions.fish

--- functions.fish
+++ functions.fish
@@ -4,19 +4,6 @@ function logs -d 'alias Nice looking log'
 	command git log -80 --color --graph --pretty=format:"%C(red)%h%C(reset)%C(yellow)%d%Creset %s %C(green italic)(%cr)%C(reset) %C(blue)%an%C(reset) %C(white dim)%GK %C(reset)" --abbrev-commit | less -R
 end
 
-function pull -d 'alias Pull changes from remote'
-	command git pull origin (current) $argv
-end
-
-function push -d 'alias Push changes to remote'
-	command git push origin (current) $argv
-end
-
-function done -d 'alias Pull and push'
-	pull
-	push
-end
-
 # Xclip commands
 function copy -d 'alias Pipe output to clipboard'
 	command xclip -selection clipboard
